2da2f05b919793a8e6a87d7515af7132736a8dbb
AAweidai <dw1253464613@gmail.com>
2022-11-29 11:39:55 +0800

    Fix setup

--- utils/setup.py
+++ utils/setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='sui-brownie',
-    version='2.3.0',
+    version='2.5.0',
     description='Sui Package Tool',
     long_description="This is an sui python tool to quickly implement sui calls",
     # The project's main homepage.
@@ -29,11 +29,7 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
     python_requires=">=3.6",
-    data_files=["sui_brownie/sui_config.template.yaml",
-                "sui_brownie/sui_keystore.template.keystore",
-                "sui_brownie/brownie-config.example.yaml",
-                "sui_brownie/README.md"
-                ],
+    include_package_data=True,
     packages=["sui_brownie"],
     install_requires=["pyyaml", "toml", "retrying", "mnemonic"]
 )
