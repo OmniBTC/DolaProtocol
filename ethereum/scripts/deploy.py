2ec3c1b448acecc2a087749bbfd815f735508be3
hacpy <hacpy.lbx@gmail.com>
2022-12-21 15:17:10 +0800

    Add send binding for evm

--- ethereum/scripts/deploy.py
+++ ethereum/scripts/deploy.py
@@ -7,7 +7,7 @@ def deploy():
     account = get_account()
 
     print("deploy bridge pool...")
-    BridgePool.deploy(
+    bridge_pool = BridgePool.deploy(
         get_wormhole(),
         get_wormhole_chain_id(),
         get_wormhole_chain_id(),
@@ -17,19 +17,27 @@ def deploy():
     )
 
     print("deploy test token btc...")
-    MockToken.deploy("BTC", "BTC", {'from': account})
+    btc = MockToken.deploy("BTC", "BTC", {'from': account})
 
     print("deploy btc pool...")
-    OmniPool.deploy(0, get_wormhole_chain_id(),
-                    BridgePool[-1].address, MockToken[-1].address, {'from': account})
+    btc_pool = OmniPool.deploy(0, get_wormhole_chain_id(),
+                               BridgePool[-1].address, MockToken[-1].address, {'from': account})
 
     print("deploy test token usdt...")
-    MockToken.deploy("USDT", "USDT", {'from': account})
+    usdt = MockToken.deploy("USDT", "USDT", {'from': account})
 
     print("deploy usdt pool...")
-    OmniPool.deploy(1, get_wormhole_chain_id(),
-                    BridgePool[-1].address, MockToken[-1].address, {'from': account})
+    usdt_pool = OmniPool.deploy(1, get_wormhole_chain_id(),
+                                BridgePool[-1].address, MockToken[-1].address, {'from': account})
 
     print("deploy lending portal...")
-    LendingPortal.deploy(BridgePool[-1].address,
-                         get_wormhole_chain_id(), {'from': account})
+    lending_portal = LendingPortal.deploy(BridgePool[-1].address,
+                                          get_wormhole_chain_id(), {'from': account})
+
+    print("----- deploy result -----")
+    print(f"bridge_pool={bridge_pool}")
+    print(f"btc={btc}")
+    print(f"btc_pool={btc_pool}")
+    print(f"usdt={usdt}")
+    print(f"usdt_pool={usdt_pool}")
+    print(f"lending_portal={lending_portal}")
