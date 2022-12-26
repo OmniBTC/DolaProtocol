7a996621158ed4aa7bc81d6d39f023b6ca42d2b5
AAweidai <dw1253464613@gmail.com>
2022-12-26 16:13:14 +0800

    fix-relayer (#35)
    * Add vote ethereum pool
    
    * Fix ethereum of relayer

--- ethereum/scripts/dola_ethereum_sdk/load.py
+++ ethereum/scripts/dola_ethereum_sdk/load.py
@@ -2,16 +2,16 @@ from dola_ethereum_sdk import DOLA_CONFIG
 
 
 def omnipool_package():
-    return DOLA_CONFIG["DOLA_ETHEREUM_PROJECT"]["OmniPool"]
+    return DOLA_CONFIG["DOLA_ETHEREUM_PROJECT"]["OmniPool"][-1]
 
 
 def wormhole_bridge_package():
-    return DOLA_CONFIG["DOLA_ETHEREUM_PROJECT"]["BridgePool"]
+    return DOLA_CONFIG["DOLA_ETHEREUM_PROJECT"]["BridgePool"][-1]
 
 
 def lending_portal_package():
-    return DOLA_CONFIG["DOLA_ETHEREUM_PROJECT"]["LendingPortal"]
+    return DOLA_CONFIG["DOLA_ETHEREUM_PROJECT"]["LendingPortal"][-1]
 
 
 def test_coins_package():
-    return DOLA_CONFIG["DOLA_ETHEREUM_PROJECT"]["MockToken"]
+    return DOLA_CONFIG["DOLA_ETHEREUM_PROJECT"]["MockToken"][-1]
