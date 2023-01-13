0502b34b3a482c484c99a4dfcc8e9b7edf444045
hacpy <hacpy.lbx@gmail.com>
2023-01-13 09:58:18 +0800

    Upgrade sui to v0.21.0 (#65)

--- sui/scripts/dola_sui_sdk/deploy.py
+++ sui/scripts/dola_sui_sdk/deploy.py
@@ -99,7 +99,7 @@ wormhole_package = sui_brownie.SuiPackage(
     is_compile=False,
     package_id=None,
     package_path=Path.home().joinpath(Path(
-        ".move/https___github_com_OmniBTC_wormhole_git_f4800d31a8cb0676b47bcf0ceb74500ccdad3b61/sui/wormhole")),
+        ".move/https___github_com_OmniBTC_wormhole_git_b1ff91995921064fc530eb8f7e147e228c10d072/sui/wormhole")),
 )
 
 wormhole_package.publish_package()
