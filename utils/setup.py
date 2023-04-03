6dcb1aa740b5c6444755e55146bd4d42f2de25c1
AAweidai <dw1253464613@gmail.com>
2023-04-03 17:44:31 +0800

    Bump sdk into version 0.29.5

--- utils/setup.py
+++ utils/setup.py
@@ -5,7 +5,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='sui-brownie',
-    version='0.29.4',
+    version='0.29.5',
     description='Sui Package Tool',
     long_description="This is an sui python tool to quickly implement sui calls",
     # The project's main homepage.
