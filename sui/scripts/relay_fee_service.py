5b4264ce5f5c5856391346b750850eeec734e253
hacpy <hacpy.lbx@gmail.com>
2023-05-31 16:33:50 +0800

    Use ipv6 for relay fee service

--- sui/scripts/relay_fee_service.py
+++ sui/scripts/relay_fee_service.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
+from flask import Flask
+
 import dola_ethereum_sdk
 import relayer
-from flask import Flask
 
 app = Flask(__name__)
 
@@ -20,4 +21,4 @@ def max_relay_fee(src_chain_id, dst_chain_id, call_name):
 if __name__ == '__main__':
     dola_ethereum_sdk.set_dola_project_path(Path("../.."))
 
-    app.run()
+    app.run(host='::', port=5000)
