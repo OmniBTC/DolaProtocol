c22a219920838fdeb0a70878d11afd8f98b916ff
hacpy <hacpy.lbx@gmail.com>
2023-06-07 17:44:57 +0800

    Get unrelayed txs because of fee not enough (#171)

--- sui/scripts/relay_fee_service.py
+++ sui/scripts/relay_fee_service.py
@@ -10,6 +10,16 @@ app = Flask(__name__)
 CORS(app)
 
 
+@app.route('/unrelay_txs/<src_chain_id>/<call_name>/<limit>')
+def unrelay_txs(src_chain_id, call_name, limit):
+    return relayer.get_unrelay_txs(src_chain_id, call_name, limit)
+
+
+@app.route('/unrelay_tx/<src_chain_id>/<sequence>')
+def unrelay_tx(src_chain_id, sequence):
+    return relayer.get_unrelay_tx_by_sequence(src_chain_id, sequence)
+
+
 @app.route('/relay_fee/<src_chain_id>/<dst_chain_id>/<call_name>/<feed_nums>')
 def relay_fee(src_chain_id, dst_chain_id, call_name, feed_nums):
     return relayer.get_relay_fee(src_chain_id, dst_chain_id, call_name, feed_nums)
