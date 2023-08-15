b130c088c0c424e37fce9d7b5b094c695adbddab
hacpy <hacpy.lbx@gmail.com>
2023-08-15 16:45:37 +0800

    Add service to return tvl

--- sui/scripts/relay_fee_service.py
+++ sui/scripts/relay_fee_service.py
@@ -4,12 +4,15 @@ from flask import Flask
 from flask_cors import CORS
 
 import dola_ethereum_sdk
+import dola_sui_sdk
 import relayer
+from dola_sui_sdk import interfaces
 
 app = Flask(__name__)
 CORS(app)
 
 dola_ethereum_sdk.set_dola_project_path(Path("../.."))
+dola_sui_sdk.set_dola_project_path("../..")
 relayer.init_markets()
 
 
@@ -33,6 +36,11 @@ def relay_fee(src_chain_id, dst_chain_id, call_name, feed_nums=0):
 @app.route('/max_relay_fee/<src_chain_id>/<dst_chain_id>/<call_name>')
 def max_relay_fee(src_chain_id, dst_chain_id, call_name):
     return relayer.get_max_relay_fee(src_chain_id, dst_chain_id, call_name)
+
+
+@app.route('/tvl')
+def total_otoken_value():
+    return {"tvl": str(interfaces.get_protocol_total_otoken_value())}
 
 
 if __name__ == '__main__':
